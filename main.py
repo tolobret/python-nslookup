41e03ae51a433ff23fa0a801d1744909ce21d488
tolobret <achmad.nofandi@gmail.com>
2023-08-21 16:30:22 +0700

    update timestamp

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import logging
 import os
 
 domain = os.environ['DOMAIN']
-
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 while(True) : 
     dns_query = Nslookup()
     try :
@@ -12,8 +12,7 @@ while(True) :
         if ips_record.answer == [] :
             logging.error('DNS Error')
         else :
-            logging.info(ips_record.response_full, ips_record.answer)
-            print(ips_record.response_full, ips_record.answer)
+            logging.info('DNS Resolved '+str(ips_record.answer))
 
     except Exception as e: 
         logging.warning(e)
